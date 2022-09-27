18e5bf0deec66cb465dde732170ea12a67998055
Lekan <olalekancrown11@gmail.com>
2022-09-27 15:57:13 -0400

    Initial commit

---
+++ server/controllers/posts.js
@@ -0,0 +1,41 @@
+const Post = require("../models/Post");
+const cloudinary = require("../middleware/cloudinary");
+// const Comment = require("../models/Comment");
+
+module.exports = {
+  getProfile: async (req, res) => {
+    try {
+      const posts = await Post.find({ user: req.user.id });
+      res.render("profile.ejs", { posts: posts, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getFeed: async (req, res) => {
+    try {
+      const posts = await Post.find().sort({ createdAt: "desc" }).lean(); //Post is the model (required above), .lean() ()is mongoose) is for getting the raw object from mongo (documents on mongo, while similar to "objects" actually include more than you need) this will be faster
+      res.render("feed.ejs", { posts: posts });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  createPost: async (req, res) => {
+    try {
+      // Upload image to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path); //upload is from the cloudinary package
+
+      await Post.create({
+        title: req.body.title,
+        image: result.secure_url, //result declared above
+        cloudinaryId: result.public_id,
+        caption: req.body.caption,
+        likes: 0,
+        user: req.user.id,
+      });
+      console.log("Post has been added!");
+      res.redirect("/profile");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+}
